f39518762ebc5d2d11786d638ce44172dd4be2e6
Salman Saghafi <salmans@users.noreply.github.com>
2019-03-27 21:54:03 -0700

    adding basic cli support (#26)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,199 @@
+use structopt::StructOpt;
+use std::fs;
+use std::io::Read;
+use rusty_razor::formula::{parser::parse_theory, syntax::Theory};
+use rusty_razor::chase::{r#impl::reference::{Sequent, Model, Evaluator},
+                         selector::{Fair, Bootstrap},
+                         strategy::FIFO,
+                         bounder::DomainSize,
+                         Observation,
+                         solve};
+
+#[derive(StructOpt)]
+enum BoundCommand {
+    #[structopt(name = "bound-off", about = "Do not bound the model.")]
+    Off,
+    #[structopt(name = "bound-domain", about = "Bound models by their domain size.")]
+    Domain {
+        #[structopt(help = "Maximum domain size of models.")]
+        size: usize,
+    },
+}
+
+#[derive(StructOpt)]
+enum ProcessCommand {
+    #[structopt(name = "solve", about = "Find models for the input theory")]
+    Solve {
+        #[structopt(short = "i", long = "input", parse(from_os_str), help = "Path to the input theory file")]
+        input: std::path::PathBuf,
+        #[structopt(subcommand, name = "bound")]
+        bound: BoundCommand,
+    }
+}
+
+#[derive(StructOpt)]
+#[structopt(name = "Rusty Razor", about = "A tool for exploring finite models of first-order theories.")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+struct Command {
+    #[structopt(subcommand, name = "command")]
+    command: ProcessCommand,
+    #[structopt(long = "no-color", help = "Make it dim.")]
+    no_color: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Command::from_args();
+    let command = args.command;
+    let no_color = args.no_color;
+
+    let mut terminal = term::stdout().unwrap();
+
+    if !no_color {
+        terminal.fg(136).unwrap();
+    }
+    println!("\n\
+    ________________________________\n\
+    ┬─┐┬ ┬┌─┐┌┬┐┬ ┬  ╦═╗╔═╗╔═╗╔═╗╦═╗\n\
+    ├┬┘│ │└─┐ │ └┬┘  ╠╦╝╠═╣╔═╝║ ║╠╦╝\n\
+    ┴└─└─┘└─┘ ┴  ┴   ╩╚═╩ ╩╚═╝╚═╝╩╚═\n\
+    -----------╦-------╦------------\n           \
+               ╟-------╢            \n");
+    terminal.reset().unwrap();
+
+    match command {
+        ProcessCommand::Solve { input, bound } => {
+            if let Some(input) = input.to_str() {
+                let theory = read_theory_from_file(input);
+                process_solve(&theory, bound, !no_color);
+            }
+        }
+    }
+}
+
+fn process_solve(theory: &Theory, bound: BoundCommand, color: bool) {
+    use rusty_razor::chase::SelectorTrait;
+    use rusty_razor::chase::StrategyTrait;
+
+    let mut terminal = term::stdout().unwrap();
+
+    if color {
+        terminal.fg(27).unwrap();
+    }
+    terminal.attr(term::Attr::Bold).unwrap();
+    println!("Finding models for theory:");
+    terminal.reset().unwrap();
+
+    theory.formulas.iter().for_each(|f| println!("  {}", f));
+
+    let theory = theory.gnf();
+    let sequents: Vec<Sequent> = theory
+        .formulas
+        .iter()
+        .map(|f| f.into()).collect();
+    let evaluator = Evaluator {};
+    let selector: Bootstrap<Sequent, Fair<Sequent>> = Bootstrap::new(sequents.iter().collect());
+    let mut strategy = FIFO::new();
+
+    let bounder = match bound {
+        BoundCommand::Off => None,
+        BoundCommand::Domain { size } => Some(DomainSize::new(size)),
+    };
+
+    strategy.add(Model::new(), selector);
+    let mut count = 0;
+
+    while !strategy.empty() {
+        solve(&mut strategy, &evaluator, bounder.as_ref(), &mut |m| { print_model(m, color, &mut count) })
+    }
+
+    println!();
+    if color {
+        terminal.fg(27).unwrap();
+    }
+    terminal.attr(term::Attr::Bold).unwrap();
+    let verb = if count == 1 { "was" } else { "were" };
+    println!("{} models {} found.", count, verb);
+    terminal.reset().unwrap();
+    println!();
+}
+
+pub fn read_theory_from_file(filename: &str) -> Theory {
+    let mut f = fs::File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    parse_theory(contents.as_str())
+}
+
+fn print_model(model: Model, color: bool, count: &mut i32) {
+    *count += 1;
+    use rusty_razor::chase::ModelTrait;
+    use itertools::Itertools;
+
+    let mut terminal = term::stdout().unwrap();
+
+    if color {
+        terminal.fg(27).unwrap();
+    }
+    terminal.attr(term::Attr::Bold).unwrap();
+
+    print!("Domain: ");
+    let domain = model.domain().iter().map(|e| e.get().to_string()).collect();
+    let fg_color = if color { None } else { Some(term::color::GREEN) };
+    print_list(&domain, fg_color);
+    println!("\n");
+
+    let elements: Vec<String> = model.domain().iter().sorted().iter().map(|e| {
+        let witnesses: Vec<String> = model.witness(e).iter().map(|w| w.to_string()).collect();
+        let witnesses = witnesses.into_iter().sorted();
+        format!("{} -> {}", witnesses.into_iter().sorted().join(", "), e.get())
+    }).collect();
+
+    print!("Elements: ");
+    let fg_color = if color { None } else { Some(term::color::BRIGHT_YELLOW) };
+    print_list(&elements, fg_color);
+    println!("\n");
+
+    let facts: Vec<String> = model.facts().iter().map(|f| {
+        match f {
+            Observation::Fact { relation, terms } => {
+                let ts: Vec<String> = terms.iter().map(|t| t.to_string()).collect();
+                format!("{}({})", relation, ts.join(", "))
+            }
+            Observation::Identity { left, right } => format!("{} = {}", left, right),
+        }
+    }).collect();
+    print!("Facts: ");
+    let fg_color = if color { None } else { Some(term::color::BRIGHT_BLUE) };
+    print_list(&facts, fg_color);
+    println!("\n");
+
+    if color {
+        terminal.fg(27).unwrap();
+    }
+    terminal.attr(term::Attr::Bold).unwrap();
+    println!("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+
+    terminal.reset().unwrap();
+}
+
+fn print_list<T: std::fmt::Display>(list: &Vec<T>, color: Option<term::color::Color>) {
+    let mut terminal = term::stdout().unwrap();
+    let last = list.len() - 1;
+    let mut index = 0;
+    for x in list {
+        if let Some(color) = color {
+            terminal.fg(color).unwrap();
+        }
+        terminal.attr(term::Attr::Bold).unwrap();
+        print!("{}", x);
+        if index != last {
+            terminal.reset().unwrap();
+            print!(", ");
+            index += 1;
+        }
+    }
+    terminal.reset().unwrap();
 }
