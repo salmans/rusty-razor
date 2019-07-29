b62b19148da22b55c09b42c261db80f507cc7093
Salman Saghafi <salman.saghafi@me.com>
2019-07-28 19:40:05 -0700

    dropping incomplete models when bounded model-finding

--- src/chase.rs
+++ src/chase.rs
@@ -232,7 +232,8 @@ pub fn solve<'s, S, M, Sel, Stg, E, B>(strategy: &mut Stg, evaluator: &E, bounde
                               parent = base_id,
                               model = tracing::field::display(&model)
                           }, "chase step applied");
-                    strategy.add(model, selector.clone());
+                    // drop the model
+                    // strategy.add(model, selector.clone());
                 }
             });
         } else {
